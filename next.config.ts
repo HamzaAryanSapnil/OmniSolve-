dd0a3f17498839467c8bf3c9597a611fb1efa495
Hamza Aryan Sapnil <hamzaswapnil@gmail.com>
2026-07-11 17:02:30 +0600

    feat: Enhance home page with filtering, add remote image support, and implement timestamp utility

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
+// images.remotePatterns i need to use this.
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com",
+      },
+    ],
+  },
   /* config options here */
 };
 
