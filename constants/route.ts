62efa98d78d8ee31b4e4cdfc11072e80566bfc29
Hamza Aryan Sapnil <hamzaswapnil@gmail.com>
2026-07-11 12:30:08 +0600

    implement local searchbar

--- constants/route.ts
+++ constants/route.ts
@@ -2,6 +2,7 @@ const ROUTES = {
   HOME: "/",
   SIGNUP: "/sign-up",
   SIGNIN: "/sign-in",
+  ASK_QUESTION: "/ask-question",
   PROFILE: (id: string) => `/profile/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
 };
